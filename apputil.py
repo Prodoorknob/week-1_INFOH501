07787bf5004c173e1d7300107f06761889ebd41a
Rajashekar Reddy Vedire <55252270+Prodoorknob@users.noreply.github.com>
2025-09-02 14:24:14 -0400

    Update apputil.py
    Functions to detect palindrome and balanced parenthesis

--- apputil.py
+++ apputil.py
@@ -1,3 +1,27 @@
 
 
 # add code below ...
+def is_palindrome(text: str) -> bool:
+    """
+    Return True if `text` is a palindrome for case-insensitive and alphanumeric strings.
+    Examples: "racecar", "A man, a plan, a canal: Panama!"
+    """
+    filtered = [char.lower() for char in text if char.isalnum()] #isalnum() checkes whether character is alphanumeric value
+    return filtered == filtered[::-1]
+
+def is_balanced_parentheses(in_value: str) -> bool:
+    """
+    Return True if parentheses/brackets/braces in `in_value` are balanced.
+    Checks (), [], {} and ignores all other characters.
+    """
+    pairs = {')': '(', ']': '[', '}': '{'}
+    openers = set(pairs.values())
+    check = []
+    for char in in_value:
+        if char in openers:
+            check.append(char)
+        elif char in pairs:
+            if not check or check[-1] != pairs[char]:
+                return False
+            check.pop()
+    return len(check) == 0
